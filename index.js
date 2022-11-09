ba4a317d9175abf6084fac99cf2fc741516d9975
Mamun2k22 <mamun180037@gmail.com>
2022-11-09 19:33:12 +0600

    2nd- Connect to Database Mongo db

--- index.js
+++ index.js
@@ -16,12 +16,16 @@ console.log(process.env.DB_PASSWORD);
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ldmt6s4.mongodb.net/?retryWrites=true&w=majority `;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
 
+// Connect to Database:
+async function run() {
+    try {
+        const serviceCollection = client.db('immigration').collection('services');
+    }
+    finally {
+
+    }
+} run().catch(er => console.error(er));
 
 app.get('/', (req, res) => {
     res.send('Immigration Server is a Running')
